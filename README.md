6059616b7d7365d49562abacfc13538a9f4ccc09
Aref <arefrazavi@gmail.com>
2024-11-04 10:30:00 -0700

    Generate graph datasets and update documentation.

--- README.md
+++ README.md
@@ -14,3 +14,42 @@
     ```
     cp .env.example .env
     ```
+
+## Graph Dataset Generation
+In order to generate graph datasets, you can run the following python script:
+```
+python3 -m graph_generation.graph_dataset_generator
+``` 
+
+The graph generation contains the following steps:
+1. Downloading data:
+We download raw Food.com Recipes and Interactions files (`shuyangli94/food-com-recipes-and-user-interactions`) from Kaggle.
+
+2. Processing users:
+In order to generate user nodes, we only retrieve their IDs from `interactions_train.csv` as they don`t contain rich properties.
+It should be noted `interactions_train.csv` contain all the users, hence there is no unseen users for validation and testing. We tested it out by running the following script:
+    ```
+    python3 -m graph_generation.compare_users
+    ```
+
+3. Processing recipes:
+In order to generate recipes nodes, we use `RAW_recipes.csv` to process all the following selected properties with proper encoders:
+    - `name`: `SequenceEncoder`
+    - `steps`: `SequenceListEncoder`
+    - `tags`: `SequenceListEncoder`
+    - `ingredients`: `SequenceListEncoder`
+    - `nutrition`: `ListEncoder`
+    - `n_steps`: `IdentityEncoder`
+    - `minutes`: `IdentityEncoder`
+
+4. Processing edges:
+In this step, we process edges with a single property (`rating`) for each data split separately. 
+    - `interactions_train.csv` contains train edges
+    - `interactions_validation.csv` contains validation edges   
+    - `interactions_test.csv` contains test edges   
+
+5. Generate graphs
+In this step, we generate graph dataset for each data split given the processed users, recipes and their respective edges.
+
+6. Storing graphs
+Finally, we store the generated graphs in PyTorch (.pt) files which can be used to training, validation and testing later. 
---
+++ graph_generation/graph_dataset_generator.py
@@ -0,0 +1,195 @@
+import os
+
+from ast import literal_eval
+from typing import Optional, Any
+
+import pandas as pd
+import numpy as np
+import torch
+from dotenv import load_dotenv; load_dotenv()
+from kaggle.api.kaggle_api_extended import KaggleApi
+from sentence_transformers import SentenceTransformer
+from torch_geometric.data import HeteroData
+from torch_geometric.transforms import ToUndirected
+
+
+class IdentityEncoder:
+    # The 'IdentityEncoder' takes the raw column values and converts them to PyTorch tensors.
+    def __init__(self, dtype: Optional[torch.dtype] = None) -> None:
+        self.dtype = dtype
+
+    def __call__(self, values: pd.Series) -> torch.Tensor:
+        return torch.from_numpy(values.to_numpy()).view(-1, 1).to(self.dtype)
+
+
+class ListEncoder:
+    # The 'SequenceEncoder' encodes a raw column of list of a given type into tensors.
+    def __init__(self, dtype: Optional[torch.dtype] = None) -> None:
+        self.dtype = dtype
+
+    def __call__(self, values: pd.Series) -> torch.Tensor:
+        return  torch.tensor(values.tolist(), dtype=self.dtype)    
+
+
+class SequenceEncoder:
+    # The 'SequenceEncoder' encodes raw column strings into embeddings tensors.
+    def __init__(self, model: SentenceTransformer, batch_size: int = 32) -> None:
+        self.model = model
+        self.batch_size = batch_size
+
+    @torch.no_grad()
+    def __call__(self, values: pd.Series) -> torch.Tensor:
+        x = self.model.encode(
+            values.to_numpy(), 
+            show_progress_bar=True,
+            convert_to_tensor=True,
+            batch_size=self.batch_size
+        )
+        return x.cpu()
+
+
+class SequenceListEncoder:
+    # The 'SequenceEncoder' encodes a raw column of list of strings into embeddings tensors.
+    def __init__(self, model: SentenceTransformer, batch_size: int = 32) -> None:
+        self.model = model
+        self.batch_size = batch_size
+
+    @torch.no_grad()
+    def __call__(self, values: pd.Series) -> torch.Tensor:
+        x = self.model.encode(
+            values.apply(', '.join).to_numpy(), 
+            show_progress_bar=True,
+            convert_to_tensor=True, 
+            batch_size=self.batch_size
+        )
+        return x.cpu()
+
+
+def download_raw_data(dataset_name: str, destination_path: str) -> None:
+    """Download raw dataset files from Kaggle."""
+    api = KaggleApi()
+    # api.authenticate()
+
+    api.dataset_download_files(dataset_name, path=destination_path, unzip=True)
+
+
+def load_sequence_embedder(model_id: str, device: Optional[str] = None) -> SentenceTransformer:
+    """Load a pre-trained embeddings model with the given ID from HugginFace for encoding sequences."""
+    return SentenceTransformer(model_id, device=device)
+
+
+def load_nodes_from_csv(file_path: str, index_col: str, encoders: Optional[dict[str, Any]] = None, **kwargs) -> tuple[torch.Tensor, list[tuple[int, int]]]:
+    df = pd.read_csv(file_path, index_col=index_col, **kwargs)
+    mapping = {index: i for i, index in enumerate(df.index.unique())}
+
+    x = None
+    if encoders is not None:
+        xs = []
+        for col, encoder in encoders.items():
+            print(f'-Encoding {col}...')
+            xs.append(encoder(df[col]))
+
+        x = torch.cat(xs, dim=-1)
+
+    return x, mapping
+
+
+def load_edges_from_csv(file_path: str, src_index_col: str, src_mapping: list[tuple[int, int]], dst_index_col: str, dst_mapping: list[tuple[int, int]], encoders: Optional[dict[str, Any]] = None, **kwargs) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
+    df = pd.read_csv(file_path, **kwargs)
+
+    src = [src_mapping[index] for index in df[src_index_col]]
+    dst = [dst_mapping[index] for index in df[dst_index_col]]
+    edge_index = torch.tensor([src, dst])
+
+    edge_label = None
+    if encoders is not None:
+        edge_attrs = []
+        for col, encoder in encoders.items():
+            print(f'-Encoding {col}...')
+            edge_attrs.append(encoder(df[col]))
+    
+        edge_label = torch.cat(edge_attrs, dim=-1)
+
+    return edge_index, edge_label
+
+
+def generate_graph(user_mapping: list[tuple[int, int]], recipe_x: torch.Tensor, edge_index: torch.Tensor, edge_label: Optional[torch.Tensor]) -> HeteroData:
+    graph_data = HeteroData()
+    graph_data['user'].num_nodes = len(user_mapping)  # Users do not have any features.
+    graph_data['recipe'].x = recipe_x
+    graph_data['user', 'rates', 'recipe'].edge_index = edge_index
+    graph_data['user', 'rates', 'recipe'].edge_label = edge_label
+    print('--Graph before ToUndirected: \n', graph_data)
+
+    # Add reverse ('recipe', 'rev_rates', 'user') relation for message passing.
+    graph_data = ToUndirected()(graph_data)
+    # Remove 'reverse' label.
+    del graph_data['recipe', 'rev_rates', 'user'].edge_label  
+
+    return graph_data
+
+
+def store_graph(graph: HeteroData, file_path: str) -> None:
+    torch.save(graph, file_path)
+
+
+if __name__ == '__main__':
+    base_data_path = 'data'
+    raw_dataset_name = 'shuyangli94/food-com-recipes-and-user-interactions'
+    sequence_embedder_id = 'sentence-transformers/all-mpnet-base-v2'
+    device = 'cuda' if torch.cuda.is_available() else None
+    batch_size = 64
+    graph_version = 1
+
+    print("Downloading raw files from Kaggle...")
+    download_raw_data(raw_dataset_name, base_data_path)
+
+    print('Loading user nodes...')
+    _, user_mapping = load_nodes_from_csv(
+        f'{base_data_path}/interactions_train.csv', 
+        index_col='user_id'
+    )
+
+    print('Loading recipe nodes...')
+    sequence_embedder = load_sequence_embedder(sequence_embedder_id, device)
+    recipe_x, recipe_mapping = load_nodes_from_csv(
+        f'{base_data_path}/RAW_recipes.csv',
+        index_col='id',
+        encoders={
+            'name': SequenceEncoder(model=sequence_embedder, batch_size=batch_size),
+            'steps': SequenceListEncoder(model=sequence_embedder, batch_size=batch_size),
+            'tags': SequenceListEncoder(model=sequence_embedder, batch_size=batch_size),
+            'ingredients': SequenceListEncoder(model=sequence_embedder, batch_size=batch_size),
+            'nutrition': ListEncoder(dtype=torch.float),
+            'n_steps': IdentityEncoder(dtype=torch.long),
+            'minutes': IdentityEncoder(dtype=torch.long)
+        },
+        converters={'steps': literal_eval, 'tags': literal_eval, 'ingredients': literal_eval, 'nutrition': literal_eval}
+    )
+
+    for data_split_name in ['train', 'validation', 'test']:
+        print(f'Generating {data_split_name} graph...')
+        print('-Loading edges...')
+        rating_path = f'{base_data_path}/interactions_{data_split_name}.csv'
+        edge_index, edge_label = load_edges_from_csv(
+            file_path=rating_path,
+            src_index_col='user_id',
+            src_mapping=user_mapping,
+            dst_index_col='recipe_id',
+            dst_mapping=recipe_mapping,
+            encoders={'rating': IdentityEncoder(dtype=torch.long)},
+        )
+
+        graph = generate_graph(
+            user_mapping=user_mapping,
+            recipe_x=recipe_x,
+            edge_index=edge_index,
+            edge_label=edge_label
+
+        )
+        print('--Final graph: \n', graph)
+
+        print('-Storing graphs...')
+        graph_path = f'{base_data_path}/graph/v{graph_version}/{data_split_name}_graph.pt'
+        store_graph(graph, graph_path)
+        print(f'{data_split_name} graph stored in {graph_path}.')
