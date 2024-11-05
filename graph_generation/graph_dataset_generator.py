cb9138966e96ff39a1e3428d4b43192af1b2dc7c
Aref <arefrazavi@gmail.com>
2024-11-05 09:07:05 -0700

    Use RandomLinkSplit to generate supervision edges for train set.

--- graph_generation/graph_dataset_generator.py
+++ graph_generation/graph_dataset_generator.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv; load_dotenv()
 from kaggle.api.kaggle_api_extended import KaggleApi
 from sentence_transformers import SentenceTransformer
 from torch_geometric.data import HeteroData
-from torch_geometric.transforms import ToUndirected
+from torch_geometric.transforms import ToUndirected, RandomLinkSplit
 
 
 class IdentityEncoder:
@@ -119,7 +119,6 @@ def generate_graph(user_mapping: list[tuple[int, int]], recipe_x: torch.Tensor, 
     graph_data['recipe'].x = recipe_x
     graph_data['user', 'rates', 'recipe'].edge_index = edge_index
     graph_data['user', 'rates', 'recipe'].edge_label = edge_label
-    print('--Graph before ToUndirected: \n', graph_data)
 
     # Add reverse ('recipe', 'rev_rates', 'user') relation for message passing.
     graph_data = ToUndirected()(graph_data)
@@ -127,6 +126,24 @@ def generate_graph(user_mapping: list[tuple[int, int]], recipe_x: torch.Tensor, 
     del graph_data['recipe', 'rev_rates', 'user'].edge_label  
 
     return graph_data
+
+def split_graph(graph_data: HeteroData, training_supervision_ratio: float, validation_ratio: float, test_ratio: float) -> dict[str, HeteroData]:
+    transform = RandomLinkSplit(
+        disjoint_train_ratio=training_supervision_ratio,
+        num_val=validation_ratio,
+        num_test=test_ratio,
+        neg_sampling_ratio=0.0,
+        edge_types=[('user', 'rates', 'recipe')],
+        rev_edge_types=[('recipe', 'rev_rates', 'user')],
+    )
+
+    train_graph, validation_graph, test_data = transform(graph_data)
+
+    return {
+        'train': train_graph, 
+        'validation': validation_graph, 
+        'test': test_graph
+    }
 
 
 def store_graph(graph: HeteroData, file_path: str) -> None:
@@ -139,18 +156,21 @@ if __name__ == '__main__':
     sequence_embedder_id = 'sentence-transformers/all-mpnet-base-v2'
     device = 'cuda' if torch.cuda.is_available() else None
     batch_size = 64
+    training_supervision_ratio = 0.2
+    validation_ratio = 0.05
+    test_ratio = 0.1
     graph_version = 1
 
     print("Downloading raw files from Kaggle...")
-    download_raw_data(raw_dataset_name, base_data_path)
+    # download_raw_data(raw_dataset_name, base_data_path)
 
-    print('Loading user nodes...')
+    print('Processing user nodes...')
     _, user_mapping = load_nodes_from_csv(
-        f'{base_data_path}/interactions_train.csv', 
+        f'{base_data_path}/RAW_interactions.csv', 
         index_col='user_id'
     )
 
-    print('Loading recipe nodes...')
+    print('Processing recipe nodes...')
     sequence_embedder = load_sequence_embedder(sequence_embedder_id, device)
     recipe_x, recipe_mapping = load_nodes_from_csv(
         f'{base_data_path}/RAW_recipes.csv',
@@ -167,29 +187,31 @@ if __name__ == '__main__':
         converters={'steps': literal_eval, 'tags': literal_eval, 'ingredients': literal_eval, 'nutrition': literal_eval}
     )
 
-    for data_split_name in ['train', 'validation', 'test']:
-        print(f'Generating {data_split_name} graph...')
-        print('-Loading edges...')
-        rating_path = f'{base_data_path}/interactions_{data_split_name}.csv'
-        edge_index, edge_label = load_edges_from_csv(
-            file_path=rating_path,
-            src_index_col='user_id',
-            src_mapping=user_mapping,
-            dst_index_col='recipe_id',
-            dst_mapping=recipe_mapping,
-            encoders={'rating': IdentityEncoder(dtype=torch.long)},
-        )
+    print('Processing edges...')
+    rating_path = f'{base_data_path}/RAW_interactions.csv'
+    edge_index, edge_label = load_edges_from_csv(
+        file_path=rating_path,
+        src_index_col='user_id',
+        src_mapping=user_mapping,
+        dst_index_col='recipe_id',
+        dst_mapping=recipe_mapping,
+        encoders={'rating': IdentityEncoder(dtype=torch.long)},
+    )
 
-        graph = generate_graph(
-            user_mapping=user_mapping,
-            recipe_x=recipe_x,
-            edge_index=edge_index,
-            edge_label=edge_label
+    print(f'Generating graph dataset...')
+    graph = generate_graph(
+        user_mapping=user_mapping,
+        recipe_x=recipe_x,
+        edge_index=edge_index,
+        edge_label=edge_label
 
-        )
-        print('--Final graph: \n', graph)
+    )
 
-        print('-Storing graphs...')
-        graph_path = f'{base_data_path}/graph/v{graph_version}/{data_split_name}_graph.pt'
+    print(f'Splitting graph dataset...')
+    graph_splits = split_graph(graph, training_supervision_ratio, validation_ratio, test_ratio)
+
+    print('Storing graph splits...')
+    for split_name, graph in graph_splits.items():
+        graph_path = f'{base_data_path}/graph/v{graph_version}/{split_name}_graph.pt'
         store_graph(graph, graph_path)
-        print(f'{data_split_name} graph stored in {graph_path}.')
+        print(f'{split_name} graph stored in {graph_path}.')
